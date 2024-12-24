2f8db9717e6c42cee56fe579fd5d5d509d29e187
rsandhumcr <rsandhumcr@gmail.com>
2024-12-24 10:03:02 +0000

    Add powershell script to switch on or off.

---
+++ switchHosts.ps1
@@ -0,0 +1,4 @@
+
+copy C:\Windows\System32\drivers\etc\hosts C:\Windows\System32\drivers\etc\hosts.bak 
+copy C:\Windows\System32\drivers\etc\hosts.orig C:\Windows\System32\drivers\etc\hosts
+ipconfig /flushdns
---
+++ switchHostsBack.ps1
@@ -0,0 +1,3 @@
+
+copy C:\Windows\System32\drivers\etc\hosts.bak C:\Windows\System32\drivers\etc\hosts
+ipconfig /flushdns
